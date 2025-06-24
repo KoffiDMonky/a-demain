a032bd82c3c7de8247698ff23796783300bd8ce4
Agénor Houessou <agenor.houessou@hotmail.fr>
2025-06-24 21:32:38 +0200

    Update de la politique de confidentialité

--- screens/PrivacyPolicyScreen.js
+++ screens/PrivacyPolicyScreen.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { TouchableOpacity, Text, ScrollView, StyleSheet, SafeAreaView, Platform, StatusBar,Linking} from "react-native";
+import {
+  TouchableOpacity,
+  Text,
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Linking,
+} from "react-native";
 
 const PrivacyPolicyScreen = () => {
+  const openURL = (url) => {
+    Linking.openURL(url).catch((err) =>
+      console.error("Impossible d'ouvrir l'URL :", err)
+    );
+  };
 
-    const openURL = (url) => {
-        Linking.openURL(url).catch((err) =>
-          console.error("Impossible d'ouvrir l'URL :", err)
-        );
-      }; 
-        
   return (
     <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={styles.container}>
@@ -16,44 +24,68 @@ const PrivacyPolicyScreen = () => {
 
         <Text style={styles.sectionTitle}>1. Respect de la vie privée</Text>
         <Text style={styles.paragraph}>
-          L’application <Text style={styles.bold}>À Demain</Text> respecte ta vie privée. Aucune donnée personnelle n’est collectée, stockée ou transmise à des tiers. Toutes les informations saisies (tâches, historiques, préférences) restent strictement locales sur ton appareil.
+          L’application <Text style={styles.bold}>À Demain</Text> respecte ta
+          vie privée. Aucune donnée personnelle n’est collectée, stockée ou
+          transmise à des tiers. Toutes les informations saisies (tâches,
+          historiques, préférences) restent strictement locales sur ton
+          appareil.
         </Text>
 
         <Text style={styles.sectionTitle}>2. Données stockées</Text>
         <Text style={styles.paragraph}>
-          Les tâches, les séries et les statistiques sont sauvegardées localement sur ton téléphone à l’aide du système de stockage sécurisé d’Expo/React Native. Aucune donnée n’est transférée vers un serveur externe.
+          Les tâches, les séries et les statistiques sont sauvegardées
+          localement sur ton téléphone à l’aide du système de stockage sécurisé
+          d’Expo/React Native. Aucune donnée n’est transférée vers un serveur
+          externe.
         </Text>
 
         <Text style={styles.sectionTitle}>3. Notifications</Text>
         <Text style={styles.paragraph}>
-          L’app peut t’envoyer des rappels si tu les as autorisés. Ces notifications sont locales et ne contiennent que des données saisies par toi. Tu peux les désactiver dans les réglages de ton téléphone.
+          L’app peut t’envoyer des rappels si tu actives les notifications. Sur
+          iOS, elles sont totalement locales. Sur Android, l’envoi de
+          notifications utilise le service Firebase Cloud Messaging (FCM),
+          fourni par Google. Cela implique que certains identifiants techniques
+          (comme le token de notification ou l’ID de l’appareil) sont transmis à
+          Google pour permettre l’acheminement des notifications. Ces données ne
+          sont pas utilisées par l’application À Demain à d’autres fins, ne sont
+          ni collectées, ni stockées, ni revendues. Tu peux désactiver les
+          notifications à tout moment dans les réglages de ton téléphone.
         </Text>
 
         <Text style={styles.sectionTitle}>4. Droits</Text>
         <Text style={styles.paragraph}>
-          Tu peux supprimer tes données en désinstallant l’application. Aucun compte ni service distant n’est utilisé, tu restes maître de tes données.
+          Tu peux supprimer tes données en désinstallant l’application. Aucun
+          compte ni service distant n’est utilisé, tu restes maître de tes
+          données.
         </Text>
 
-        <TouchableOpacity onPress={() => openURL("https://ademain.agenorhouessou.fr/privacy")}>
-        <Text style={styles.link}>📎 Voir la politique complète</Text>
-      </TouchableOpacity>
-
+        <TouchableOpacity
+          onPress={() => openURL("https://ademain.agenorhouessou.fr/privacy")}
+        >
+          <Text style={styles.link}>📎 Voir la politique complète</Text>
+        </TouchableOpacity>
 
         <Text style={styles.title}>Mentions légales</Text>
 
         <Text style={styles.paragraph}>
           <Text style={styles.bold}>Nom de l’application :</Text> À Demain{"\n"}
           <Text style={styles.bold}>Éditeur :</Text> Agénor Houessou{"\n"}
-          <Text style={styles.bold}>E-mail :</Text> agenorhouessou@hotmail.fr{"\n"}
-          <Text style={styles.bold}>Technologies :</Text> Expo, React Native, EAS
-          <TouchableOpacity onPress={() => openURL("https://ademain.agenorhouessou.fr/terms")}>
-                <Text style={styles.link}>📎 Voir les mentions légales</Text>
-            </TouchableOpacity>
+          <Text style={styles.bold}>E-mail :</Text> agenorhouessou@hotmail.fr
+          {"\n"}
+          <Text style={styles.bold}>Technologies :</Text> Expo, React Native,
+          EAS
+          <TouchableOpacity
+            onPress={() => openURL("https://ademain.agenorhouessou.fr/terms")}
+          >
+            <Text style={styles.link}>📎 Voir les mentions légales</Text>
+          </TouchableOpacity>
         </Text>
 
         <Text style={styles.sectionTitle}>Propriété intellectuelle</Text>
         <Text style={styles.paragraph}>
-          Tous les contenus textuels, visuels et fonctionnels de l’app sont protégés par les droits d’auteur. Toute reproduction, même partielle, est interdite sans autorisation écrite.
+          Tous les contenus textuels, visuels et fonctionnels de l’app sont
+          protégés par les droits d’auteur. Toute reproduction, même partielle,
+          est interdite sans autorisation écrite.
         </Text>
 
         <Text style={styles.sectionTitle}>Contact</Text>
@@ -100,7 +132,7 @@ const styles = StyleSheet.create({
   bold: {
     fontWeight: "bold",
   },
-link: {
+  link: {
     fontSize: 16,
     color: "#FF2E54",
     textDecorationLine: "underline",
